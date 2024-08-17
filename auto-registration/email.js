47045b310321dad02b3f4d3aebfa464b988b7739
zahinafsar <afsarzahin@gmail.com>
2024-08-17 13:52:51 +0600

    update registration automation

--- auto-registration/email.js
+++ auto-registration/email.js
@@ -1,19 +1,43 @@
-const sendGridMail = require("@sendgrid/mail");
-sendGridMail.setApiKey(process.env.SEND_GRID);
+// const sendGridMail = require("@sendgrid/mail");
+// sendGridMail.setApiKey(process.env.SEND_GRID);
 
-exports.sendEmail = async ({ subject, availableSeat }) => {
-  try {
-    await sendGridMail.send({
-      from: {
-        name: "Pre-registration bot",
-        email: "contact@findinghealers.com",
-      },
-      to: "afsarzahin@gmail.com",
-      subject: `Seat Open for ${subject}!`,
-      html: `<strong>We found ${availableSeat} available seat for ${subject}. Hurry up and take it.</strong>`,
-    });
-    console.log(`Email sent to ${to}...`);
-  } catch (error) {
-    console.log("SendGrid", "Something went wrong while sending email.");
-  }
-};
+// exports.sendEmail = async ({ subject, availableSeat }) => {
+//   try {
+//     await sendGridMail.send({
+//       from: {
+//         name: "Pre-registration bot",
+//         email: "contact@findinghealers.com",
+//       },
+//       to: "afsarzahin@gmail.com",
+//       subject: `Seat Open for ${subject}!`,
+//       html: `<strong>We found ${availableSeat} available seat for ${subject}. Hurry up and take it.</strong>`,
+//     });
+//     console.log(`Email sent to ${to}...`);
+//   } catch (error) {
+//     console.log("SendGrid", "Something went wrong while sending email.");
+//   }
+// };
+
+
+// const sections = [
+//   {
+//       courseName: "Microprocessors",
+//       courseId: 454,
+//       sectionCode: "222_D1",
+//   },
+//   {
+//       courseName: "Computer Networking",
+//       courseId: 462,
+//       sectionCode: "221_D7",
+//   },
+//   {
+//       courseName: "Computer and Cyber Security",
+//       courseId: 474,
+//       sectionCode: "221_D3",
+//   },
+//   {
+//       courseName: "Computer Networking Lab",
+//       courseId: 463,
+//       sectionCode: "221_D20",
+//   },
+// ];
--- auto-registration/index.js
+++ auto-registration/index.js
@@ -1,139 +1,101 @@
 require("dotenv").config();
 
 const puppeteer = require("puppeteer");
-const axios = require("axios");
-const { sendEmail } = require("./email");
-const { format } = require("date-fns");
+
+const config = {
+    acaCalId: 72,
+    programId: 2,
+    versionId: 1,
+    studentId: 24134,
+}
 
 const sections = [
-  {
-    courseName: "Compiler Lab",
-    courseId: 457,
-    sectionCode: "222_D5",
-  },
+    {
+        courseName: "Microprocessors",
+        courseId: 454,
+        sectionCode: "222_D1",
+    },
+    {
+        courseName: "Computer Networking",
+        courseId: 462,
+        sectionCode: "221_D7",
+    },
 ];
 
 const main = async () => {
-  console.log("Running on", format(new Date(), "hh:mm a"));
-  const browser = await puppeteer.launch({
-    headless: "new",
-    executablePath: "/usr/bin/chromium-browser",
-    args: ["--no-sandbox"],
-  });
-  const page = await browser.newPage();
-  const timeout = 20000;
-  page.setDefaultTimeout(timeout);
-
-  try {
-    await page.goto("https://studentportal.green.edu.bd/");
-    await page.waitForSelector(`#Input_LoginId`, { timeout, visible: true });
-
-    await page.type(`#Input_LoginId`, "221902014");
-    await page.type(`#Input_Password`, process.env.PASSWORD);
-
-    await Promise.all([page.click(`button`), page.waitForNavigation()]);
-
-    const cookies = await page.cookies();
-
-    const headers = {
-      accept: "application/json, text/plain, */*",
-      "accept-language": "en-US,en;q=0.9,bn;q=0.8",
-      "sec-ch-ua":
-        '"Google Chrome";v="119", "Chromium";v="119", "Not?A_Brand";v="24"',
-      "sec-ch-ua-mobile": "?0",
-      "sec-ch-ua-platform": '"macOS"',
-      "sec-fetch-dest": "empty",
-      "sec-fetch-mode": "cors",
-      "sec-fetch-site": "same-origin",
-      cookie: cookies.map((c) => `${c.name}=${c.value}`).join("; "),
-      Referer:
-        "https://studentportal.green.edu.bd/Student/StudentSectionSelection",
-      "Referrer-Policy": "strict-origin-when-cross-origin",
-    };
-
-    const allCourse = await getCourses(headers);
-
-    sections.forEach(async (section) => {
-      const { data } = await axios.post(
-        `https://studentportal.green.edu.bd/api/CourseSectionInfo?acaCalId=71&programId=2&courseId=${section.courseId}&versionId=1`,
-        null,
-        {
-          headers,
-        }
-      );
-      const open = data.find((d) => d.sectionName === section.sectionCode);
-      const course = allCourse.find((c) => c.courseID === section.courseId);
-      if (course.sectionName === null) {
-        if (open?.capacity > open?.occupied) {
-          console.log("Seat available for", section.courseName);
-          selectSection(headers, {
-            sheetId: course.regWorksheetId,
-            sectionId: open.acaCal_SectionID,
-            sectionName: section.sectionCode,
-            courseCode: open.formalCode,
-            courseId: section.courseId,
-          });
-          console.log("Section selected for", section.courseName);
-          sendEmail({
-            subject: section.courseName,
-            availableSeat: open.capacity - open.occupied,
-          });
-        } else {
-          console.log("No seat available for", section.courseName);
-        }
-      } else {
-        console.log("You already got a section for", section.courseName);
-      }
+    console.log("Running on", new Date().toLocaleTimeString());
+    const browser = await puppeteer.launch({
+        headless: "new",
+        // executablePath: "/usr/bin/chromium-browser",
+        args: ["--no-sandbox"],
     });
-  } catch (err) {
-    console.log("Failed to run the script");
-  } finally {
-    await browser.close();
-  }
-};
+    const page = await browser.newPage();
+    const timeout = 20000;
+    page.setDefaultTimeout(timeout);
 
-const selectSection = async (
-  headers,
-  { sectionId, sectionName, courseCode, courseId, sheetId }
-) => {
-  const url = "https://studentportal.green.edu.bd/api/SectionTake";
-  const params = new URLSearchParams();
-  params.append("regWorkSheetId", sheetId);
-  params.append("newSectionId", sectionId);
-  params.append("sectionName", sectionName);
-  params.append("studentId", "24134");
-  params.append("courseCode", courseCode);
-  params.append("courseId", courseId);
-  params.append("versionId", "1");
-  params.append("programId", "2");
-  params.append(
-    "url",
-    "https://studentportal.green.edu.bd/Student/StudentSectionSelection"
-  );
+    try {
+        await page.goto("https://studentportal.green.edu.bd/");
+        await page.waitForSelector(`#Input_LoginId`, { timeout, visible: true });
+        await page.type(`#Input_LoginId`, process.env.STUDENT_ID);
+        await page.type(`#Input_Password`, process.env.PASSWORD);
 
-  const urlWithParams = url + "?" + params.toString();
+        await Promise.all([page.click(`button`), page.waitForNavigation()]);
 
-  await axios.post(urlWithParams, null, {
-    headers,
-  });
-};
+        const api = (url, params) => page.evaluate(async (url, params) => {
+            const newURL = url + "?" + new URLSearchParams(params).toString();
+            return fetch(newURL, {
+                "method": "POST",
+                "credentials": "include"
+            }).then(response => response.json());
+        }, url, params);
 
-const getCourses = async (headers) => {
-  const { data } = await axios.post(
-    "https://studentportal.green.edu.bd/api/AutoAssignCourse?studentId=24134",
-    null,
-    {
-      headers,
-      referrer:
-        "https://studentportal.green.edu.bd/Student/StudentSectionSelection",
-      referrerPolicy: "strict-origin-when-cross-origin",
-      body: null,
-      method: "POST",
-      mode: "cors",
-      credentials: "include",
+        const allCourse = await api(
+            "https://studentportal.green.edu.bd/api/AutoAssignCourse",
+            { studentId: config.studentId }
+        );
+
+        sections.forEach(async (section) => {
+            const data = await api(
+                "https://studentportal.green.edu.bd/api/CourseSectionInfo",
+                {
+                    acaCalId: config.acaCalId,
+                    programId: config.programId,
+                    courseId: section.courseId,
+                    versionId: config.versionId,
+                }
+            );
+
+            const open = data.find((d) => d.sectionName === section.sectionCode);
+            const course = allCourse.find((c) => c.courseID === section.courseId);
+
+            if (course.sectionName === null) {
+                if (open?.capacity > open?.occupied) {
+                    console.log("Seat available for", section.courseName);
+                    await api(
+                        "https://studentportal.green.edu.bd/api/SectionTake",
+                        {
+                            regWorkSheetId: course.regWorksheetId,
+                            newSectionId: open.acaCal_SectionID,
+                            sectionName: section.sectionCode,
+                            studentId: config.studentId,
+                            courseCode: open.formalCode,
+                            courseId: section.courseId,
+                            versionId: config.versionId,
+                            programId: config.programId,
+                            url: "https://studentportal.green.edu.bd/Student/StudentSectionSelection",
+                        }
+                    );
+                    console.log("Section selected for", section.courseName);
+                } else {
+                    console.log("No seat available for", section.courseName);
+                }
+            } else {
+                console.log("You already got a section for", section.courseName);
+            }
+        });
+    } catch (error) {
+        console.log("Error", error);
     }
-  );
-  return data;
 };
 
 main();
